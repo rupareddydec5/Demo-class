451b80d6f14c58dfccc3be8e843d1f3096972eea
dsrak <dsrak@DESKTOP-IAFPSLH>
2020-03-17 17:14:12 -0400

    first commit

---
+++ Demo-class/src/demopackage/democlass.java
@@ -0,0 +1,18 @@
+package demopackage;
+
+//public class democlass {
+//	public static void main(String[] args) {
+//		System.out.println("HELLO");
+//	}
+
+//}
+
+
+
+public class democlass {
+	public static void main(String[] args) {
+		String a = "Rupa";
+	System.out.println("Welcome to Java sel world " + a); 
+	}
+	
+}
